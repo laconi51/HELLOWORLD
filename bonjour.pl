f62b4ccb8b2887a75c68df827a06294dd0bd5fef
Nicolas GIGNON <nicolas.gignon@laposte.fr>
2017-02-10 20:44:39 +0100

    Ajout entete

--- bonjour.pl
+++ bonjour.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
-
+##############################
+#	script qui ne sert a rien
+#   cree le 2016/01/02
+#   auteur  bidon
 
 for (my $i = 0; $i <= 9; $i++) {
    print "Hello github" : $i\n";
