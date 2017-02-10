f096dce8e2deb93fd75d5d935a2f7fb5baa87c84
Nicolas GIGNON <nicolas.gignon@laposte.fr>
2017-02-10 20:45:10 +0100

    Suppression entete

--- bonjour.pl
+++ bonjour.pl
@@ -1,8 +1,5 @@
 #!/usr/bin/perl
-##############################
-#	script qui ne sert a rien
-#   cree le 2016/01/02
-#   auteur  bidon
+
 
 for (my $i = 0; $i <= 9; $i++) {
    print "Hello github" : $i\n";
